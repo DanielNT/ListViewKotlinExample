7e30ebf364fb7912a84a43fff240e1f0198e152d
DanielNT <dan@NT.com>
2017-08-11 23:18:06 +0200

    Added pattern ViewHolder

--- app/src/main/java/com/example/danwe/pruebastutorial/AdaptadorTitulares.kt
+++ app/src/main/java/com/example/danwe/pruebastutorial/AdaptadorTitulares.kt
@@ -10,19 +10,37 @@ import android.widget.TextView
 /**
  * Created by DanielNT on 11/08/2017.
  */
+
 class AdaptadorTitulares(context: Context, val datos: Array<Titular>)
     : ArrayAdapter<Titular>(context, R.layout.listitem_titular, datos) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val inflater = LayoutInflater.from(context)
-        val item: View =  inflater.inflate (R.layout.listitem_titular, null)
+        var item = convertView // will be reasigned, need to assign to a new var (mutable)
+        val holder: ViewHolder //pattern ViewHolder
+        if (item == null) {
+            val inflater = LayoutInflater.from(context)
+            item = inflater.inflate(R.layout.listitem_titular, null)
 
-        val lblTitulo = item.findViewById<TextView>(R.id.lblTitulo)
-        lblTitulo.text = datos[position].titulo
-        val lblSubtitulo = item.findViewById<TextView>(R.id.lblSubTitulo)
-        lblSubtitulo.text = datos[position].subtitulo
+            holder = ViewHolder()
+            holder.titulo = item.findViewById<TextView>(R.id.lblTitulo)
+            holder.subtitulo = item.findViewById<TextView>(R.id.lblSubTitulo)
 
-        return item
+            item.tag = holder
+        } else {
+            holder = item.tag as ViewHolder
+        }
+
+        holder.titulo!!.text = datos[position].titulo
+        holder.subtitulo!!.text = datos[position].subtitulo
+
+        return item!!
+    }
+
+    internal class ViewHolder {
+        var titulo: TextView? = null
+        var subtitulo: TextView? = null
     }
 
 }
+
+
